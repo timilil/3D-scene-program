38c20ffdde1197ee3f4a4443d82ac0c9df5bb847
timilil <timi.liljestrom@hotmail.com>
2019-03-29 18:11:24 -0600

    Modified the movable object appearance

--- src/javascripts/project.js
+++ src/javascripts/project.js
@@ -27,6 +27,9 @@ export function displayScene(){
             texture.repeat.set(1, 1)*/
             renderer.render(scene, camera)
         }),
+        playerHead: textureLoader.load('./images/male_face.png', function(){
+            renderer.render(scene, camera)
+        }),
         
 
     }
@@ -53,24 +56,73 @@ export function displayScene(){
     water.rotateX(-(Math.PI / 2))
     scene.add(water)
 
-    // basketball
-    //geometry = new THREE.SphereGeometry(15, 40, 60)
-    //material = new THREE.MeshNormalMaterial({color: 0xFF00FF})
-    //let basketball = new THREE.Mesh(geometry/*, material*/)
-    /*basketball.materialParams = {}
-    basketball.name = "basketball"
-    basketball.translateX(30)
-    basketball.translateZ(40)
-    basketball.translateY(15)
-    scene.add(basketball)*/
+    geometry = new THREE.BoxGeometry(10, 40, 10)
+    let playerLeftLeg = new THREE.Mesh(geometry)
+    //let material = new THREE.MeshBasicMaterial({ color: 0x0000FF})
+    playerLeftLeg.materialParams = {color: 0x00FF00}
+    playerLeftLeg.position.set(0, 20, -10)
+    playerLeftLeg.name = "playerLeftLeg"
+    //scene.add(playerLeftLeg)
 
-    geometry = new THREE.BoxGeometry(30, 30, 30)
-    //material = new THREE.MeshPhongMaterial({color: 0x00FF00})
-    let player = new THREE.Mesh(geometry)
-    player.materialParams = {/*color: 0x00FF00*/}
-    player.position.set(0, 15, 0)
-    player.name = "crate"
-    scene.add(player)
+    geometry = new THREE.BoxGeometry(10, 40, 10)
+    let playerRightLeg = new THREE.Mesh(geometry)
+    playerRightLeg.materialParams = {}
+    playerRightLeg.position.set(0, 20, 10)
+    playerRightLeg.name = "playerRightLeg"
+
+    geometry = new THREE.BoxGeometry(20, 50, 30)
+    let playerTorso = new THREE.Mesh(geometry)
+    playerTorso.materialParams = {}
+    playerTorso.position.set(0, 65, 0)
+    playerTorso.name = "playerTorso"
+
+    geometry = new THREE.BoxGeometry(10, 35, 10)
+    let playerLeftArm = new THREE.Mesh(geometry)
+    playerLeftArm.materialParams = {}
+    playerLeftArm.rotateX(10)
+    playerLeftArm.position.set(0, 72, -20)
+    playerLeftArm.name = "playerLeftArm"
+
+    geometry = new THREE.BoxGeometry(10, 35, 10)
+    let playerRightArm = new THREE.Mesh(geometry)
+    playerRightArm.materialParams = {}
+    playerRightArm.rotateX(-10)
+    playerRightArm.position.set(0, 72, 20)
+    playerRightArm.name = "playerRightArm"
+
+    geometry = new THREE.SphereGeometry(15, 30, 30)
+    let playerHead = new THREE.Mesh(geometry)
+    playerHead.materialParams = {}
+    playerHead.position.set(0, 104, 0)
+    //playerHead.material = new THREE.MeshPhongMaterial(playerHead.materialParams)
+    //playerHead.material.map = textures.playerHead
+    playerHead.name = "playerHead"
+    //scene.add(playerHead)
+
+    let playerGeometry = new THREE.Geometry();
+    playerLeftLeg.updateMatrix()
+    playerGeometry.merge(playerLeftLeg.geometry, playerLeftLeg.matrix)
+
+    playerRightLeg.updateMatrix()
+    playerGeometry.merge(playerRightLeg.geometry, playerRightLeg.matrix)
+    
+    playerTorso.updateMatrix()
+    playerGeometry.merge(playerTorso.geometry, playerTorso.matrix)
+
+    playerHead.updateMatrix()
+    playerGeometry.merge(playerHead.geometry, playerHead.matrix)
+
+    playerLeftArm.updateMatrix()
+    playerGeometry.merge(playerLeftArm.geometry, playerLeftArm.matrix)
+
+    playerRightArm.updateMatrix()
+    playerGeometry.merge(playerRightArm.geometry, playerRightArm.matrix)
+    
+    //playerGeometry.materialParams = {}
+    
+    let player = new THREE.Mesh(playerGeometry)
+    player.materialParams = {}
+    scene.add(player);
 
     // add scene to html page
     canvas.appendChild(renderer.domElement)
@@ -83,7 +135,8 @@ export function displayScene(){
 
     let cameraControls = new OrbitControls(camera, renderer.domElement)
     cameraControls.addEventListener("change", function(){
-        //camera.lookAt(player.position) 
+        
+        camera.lookAt(player.position) 
         renderer.render(scene, camera)
     })
     // limit the camera so it wont go "below" ground
@@ -94,9 +147,9 @@ export function displayScene(){
     camera.position.y = controls.radius * Math.cos(controls.theta) 
     camera.position.z = controls.radius * Math.sin(controls.theta) * Math.sin(controls.phi)*/
 
-    camera.position.x = -300
-    camera.position.y = 200
-    camera.position.z = 0
+    camera.position.z = player.position.z 
+    camera.position.x = player.position.x -400
+    camera.position.y = player.position.y +400
     
     let ambientLight = new THREE.AmbientLight(0x333333)
     let directionalLight = new THREE.DirectionalLight(0x777777)
@@ -118,9 +171,9 @@ export function displayScene(){
 
     let lastMove = null
     let insideBoundary = true
+    let walkSpeed = 5
     document.onkeydown = function(e) {
         //console.log(e.keyCode)
-        // check that the movable object is inside the island
         switch(e.keyCode){
             case 87: 
                 if (!insideBoundary && lastMove === "forward") {
@@ -128,10 +181,14 @@ export function displayScene(){
                 } else {
                     console.log("move forward")
                     lastMove = "forward"
-                    player.translateX(10)
+                    player.translateX(walkSpeed)
                     camera.lookAt(player.position)
-                    //camera.position.x = camera.position.x + 10
-                    renderer.render(scene, camera)
+                    //camera.position.z = player.position.z 
+                    //camera.position.x = player.position.x -400
+                    //camera.position.y = player.position.y +400
+                    //camera.position.set(player.position.x -400, player.position.y +400, player.position.z);
+                    
+                    //cameraControls.update();
                 } 
                 break
             case 83: 
@@ -140,10 +197,12 @@ export function displayScene(){
                 } else {
                     console.log("move backwards")
                     lastMove = "backward"
-                    player.translateX(-10)
+                    player.translateX(-walkSpeed)
                     camera.lookAt(player.position)
+                    //camera.position.z = player.position.z 
+                    //camera.position.x = player.position.x -400
+                    //camera.position.y = player.position.y +400
                     //camera.position.x = camera.position.x - 10
-                    renderer.render(scene, camera)
                 } 
                 break
             case 65: 
@@ -155,7 +214,6 @@ export function displayScene(){
                     // these 2 lines will make the player move to the correct direction after rotating
                     player.rotateY(2)
                     player.rotateOnWorldAxis(new THREE.Vector3(0, 1, 0), -(Math.PI/2))
-                    renderer.render(scene, camera)
                 }
                 break
             case 68: 
@@ -168,10 +226,12 @@ export function displayScene(){
                     player.rotateY(-2)
                     
                     player.rotateOnWorldAxis(new THREE.Vector3(0, 1, 0), Math.PI/2)
-                    renderer.render(scene, camera)
                 }
                 break
-        } 
+        }
+        renderer.render(scene, camera) 
+
+        // check that the movable object is inside the island boundary
         if((Math.pow((player.position.x - island.position.x), 2) + Math.pow((player.position.z - island.position.z), 2)) < Math.pow(islandRadius, 2)){
             insideBoundary = true
         } else {
